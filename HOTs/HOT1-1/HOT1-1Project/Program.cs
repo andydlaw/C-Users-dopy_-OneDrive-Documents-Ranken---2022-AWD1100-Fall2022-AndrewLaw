f7a1877b79aa78c61fcf81e12e18a5b6c953907e
Andrew Law <andrew_law@insideranken.org>
2022-08-27 11:15:06 -0500

    First HOT included

---
+++ HOTs/HOT1-1/HOT1-1Project/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * EX1 (25pts)
+
+Write a C# program named HOT1-1 that asks your name and prints the following message:
+
+Hello, yourName! (where yourName is your full name).
+*/
+namespace HOT1_1Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string yourName;
+
+            Console.Write("Please Enter Your Full Name: ");
+            yourName = Console.ReadLine();
+
+            Console.WriteLine("\nHello, " + yourName + "!");
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ HOTs/HOT1-3/HOT1-3Project/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * EX3 (50pts)
+
+Write a C# program named HOT1-3 that prompts the user for a name, street address, city, state, zip code, and quantity of t-shirts ordered at $14.99 each. In an attractive format (similar to the one below), display all the input data as well as the following:
+
+● Total Price: defined as number ordered times price each
+
+● Sales tax: defined as 8% of the total price
+
+● Amount due: defined as amount due before tax, plus tax
+*/
+
+namespace HOT1_3Project
+{
+    internal class Program
+    {
+        //Program Constants
+        const decimal shirtPrice = 14.99m;
+        const decimal taxRate = 0.08m;
+        static void Main(string[] args)
+        {
+            string fullName;
+            string address;
+            string city;
+            string zip;
+            string state;
+            int numTshirtBought;
+            decimal subTotal;
+            decimal taxTotal;
+            decimal finalTotal;
+
+            Console.Write("Enter your name: ");
+            fullName = Console.ReadLine();
+            Console.Write("Street address: ");
+            address = Console.ReadLine();
+            Console.Write("City: ");
+            city = Console.ReadLine();
+            Console.Write("State: ");
+            state = Console.ReadLine();
+            Console.Write("Zip: ");
+            zip = Console.ReadLine();
+            Console.Write("How many T-Shirts did you want to order: ");
+            numTshirtBought = Convert.ToInt32(Console.ReadLine());
+
+
+            //Calculate Subtotal
+            subTotal = numTshirtBought * shirtPrice;
+            taxTotal = subTotal * taxRate;
+            finalTotal = subTotal + taxTotal;
+
+
+            Console.WriteLine("\n\nReciept for:");
+            Console.WriteLine(fullName);
+            Console.WriteLine(address);
+            Console.WriteLine(city + ", " + state + " " + zip);
+            Console.WriteLine("\n" + numTshirtBought + " T-Shirts ordered @ $14.99 each");
+            Console.WriteLine("\nTotal:       " + subTotal.ToString("c"));
+            Console.WriteLine("Tax:          " + taxTotal.ToString("c"));
+            Console.WriteLine("--------------------");
+            Console.WriteLine("Due:         " + finalTotal.ToString("c"));
+            
+
+
+
+
+
+
+        }
+    }
+}
